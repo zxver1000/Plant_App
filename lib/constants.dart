b672efed13f312b64b28a3e66efee0cde74403c9
seol11796 <89733207+seol11796@users.noreply.github.com>
2022-05-06 21:14:04 +0900

    seol

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFD0FA58);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'main2.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'chat.dart';
-import 'package:hci/Notice_board.dart';
-final firestore = FirebaseFirestore.instance;
+import 'routes.dart';
+import 'screens/splash/splash_screen.dart';
+import 'theme.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const MyApp());
+void main() {
+  runApp(MarketKurly());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int tab=0;
-
+class MarketKurly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:
-      Scaffold(
-      appBar: AppBar(title:  Text('HCI'),actions: [
-        IconButton(onPressed: (){
-
-        },icon: Icon(Icons.send))
-      ],),
-
-      body: [Text('home'),Text('레시피'),Main2(),board()][tab],
-        bottomNavigationBar: BottomNavigationBar(
-          type:BottomNavigationBarType.fixed,
-            selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white.withOpacity(.30),
-          currentIndex: tab,
-          backgroundColor: Colors.grey,
-
-          onTap: (i){
-            setState(() {
-              tab=i;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-              
-                label: '홈',
-                icon: Icon(Icons.home),
-
-            ),BottomNavigationBarItem(
-
-              label: '정보',
-              icon: Icon(Icons.home),
-
-            )
-          ,BottomNavigationBarItem(
-                label: '레시피',
-                icon: Icon(Icons.shopping_bag),
-            ),
-            BottomNavigationBarItem(
-        label: '정보자랑게시판',
-        icon: Icon(Icons.library_books),
-
-      ),]
-        ) ,
-    ));
+      debugShowCheckedModeBanner: false,
+      title: "Market Kurly UI",
+      // (1)
+      initialRoute: SplashScreen.routeName,
+     // (2)
+      routes: route,
+      // (3)
+      theme: theme(),
+    );
   }
 }
 
