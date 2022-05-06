5bbefc28fcd30d988a62e82525d32088ccec4e3d
seol11796 <89733207+seol11796@users.noreply.github.com>
2022-05-06 21:14:40 +0900

    seol

---
+++ lib/models/grid_category_menu.dart
@@ -0,0 +1,16 @@
+class GridCategoryMenu {
+  String title;
+  String image;
+
+  GridCategoryMenu({required this.title, required this.image});
+
+}
+
+List gridCategoryMenuList = [
+  GridCategoryMenu(title: "알로에", image: "assets/images/알로에.jpg"),
+  GridCategoryMenu(title: "아기 사과", image: "assets/images/아기사과.jpg"),
+  GridCategoryMenu(title: "상추", image: "assets/images/상추.jpg"),
+  GridCategoryMenu(title: "깻잎", image: "assets/images/깻잎.jpg"),
+  GridCategoryMenu(title: "방울 토마토", image: "assets/images/방울-토마토.jpg"),
+  GridCategoryMenu(title: "파", image: "assets/images/파.jpg"),
+];
