6789c5e771d295c010c249f07c2919f741b1c097
jj-hyunee <jj.hyun52.6@gmail.com>
2022-05-22 20:39:33 +0900

    add firebase initialize option

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ import 'screens/splash/splash_screen.dart';
 import 'theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(MarketKurly());
 }
 
