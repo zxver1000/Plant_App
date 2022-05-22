a027de8fdb1203223b244443f4df5734a571b7db
seol11796 <89733207+seol11796@users.noreply.github.com>
2022-05-22 14:57:23 +0900

    Update nav_item.dart

--- lib/models/nav_item.dart
+++ lib/models/nav_item.dart
@@ -14,6 +14,7 @@ NaveItem(label: "홈", id: 0, icon: "assets/icons/home.svg"),
  // NaveItem(label: "추천", id: 1, icon: "assets/icons/star.svg"),
   NaveItem(label: "레시피" , id:1, icon: "assets/icons/square.svg"),
   NaveItem(label: "카테고리", id: 2, icon: "assets/icons/loupe.svg"),
-  NaveItem(label: "게시판", id: 3, icon: "assets/icons/user.svg")
+  NaveItem(label: "게시판", id: 3, icon: "assets/icons/board.svg"),
+  NaveItem(label: "마이페이지", id: 4, icon: "assets/icons/user.svg")
 
 ];
