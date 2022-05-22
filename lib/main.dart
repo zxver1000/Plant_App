5d572d02f74478c3a593aa07228da61ddbc367a6
seol11796 <89733207+seol11796@users.noreply.github.com>
2022-05-22 10:28:26 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,15 @@ import 'screens/splash/splash_screen.dart';
 import 'theme.dart';
 
 void main() {
-  runApp(MarketKurly());
+  runApp(PlantApp());
 }
 
-class MarketKurly extends StatelessWidget {
+class PlantApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Market Kurly UI",
+      title: "Plant app UI",
       // (1)
       initialRoute: SplashScreen.routeName,
      // (2)
