df4ff650e1a48ece2fcf7098650552d6a73691fe
seol11796 <89733207+seol11796@users.noreply.github.com>
2022-05-22 19:09:21 +0900

    Update main_screens.dart

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -9,6 +9,7 @@ import 'package:plant_app/models/nav_item.dart';
 import 'package:plant_app/constants.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/Notice_board.dart';
+import 'package:plant_app/screens/recipe/recipe.dart';
 
 class MainScreens extends StatefulWidget {
   static String routeName = "/main_screens";
@@ -29,6 +30,7 @@ class _MainScreensState extends State<MainScreens> {
         index: _selectedIndex,
         children: [
           HomeScreen(),
+          recipe(),
           RecommendScreen(),
           CategoryScreen(),
           board(),
